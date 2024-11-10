c89f8c6916ae244fb486665e59c4028411b3db5e
daikitakaki <162309554+takakidaiki@users.noreply.github.com>
2024-11-10 05:44:55 +0000

    showページ完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,32 +1,50 @@
 class BooksController < ApplicationController
+
   def index
-    @book = Book.new
     @books = Book.all
-  end
-  
-  def create
-    @book = Book.new(book_params)
-    @book.save
-    redirect_to book_path(@book.id)
+    @book = Book.new
   end
 
   def show
-    @book = Book.find(params{:id})
+    @book = Book.find(params[:id])
   end
 
   def edit
-    @book = Book.find(params{:id})
+    @book = Book.find(params[:id])
   end
-  
+
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:alert]
+      render :edit
+    end
   end
-  
+
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
-  
-  private
-  
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:alert]
+      @books = Book.all
+      render :index
+    end
+  end
+
+
+private
   def book_params
-    book.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 end
